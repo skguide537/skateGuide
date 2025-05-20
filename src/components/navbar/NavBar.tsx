11212dd1f51d103fc8274a99ddba8296e0beae09
Omri <irmoomri@gmail.com>
2025-05-20 10:22:14 +0300

    Add Loading component and apply across map, add-spot, modal, and navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -15,15 +15,21 @@ import Avatar from '@mui/material/Avatar';
 import { useRouter } from 'next/navigation';
 import { DEFAULT_AVATAR_URL } from '@/types/constants';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Loading from "@/components/loading/Loading";
 
 =======
 >>>>>>> 776b990 (Add Cloudinary fallback avatar support and dynamic NavBar user state)
+=======
+import Loading from "@/components/loading/Loading";
+
+>>>>>>> ebac5ed (Add Loading component and apply across map, add-spot, modal, and navbar)
 
 
 export default function NavBar() {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
     const [user, setUser] = React.useState<any>(null);
+<<<<<<< HEAD
 <<<<<<< HEAD
     const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
@@ -52,13 +58,16 @@ export default function NavBar() {
 if (isLoading) return <Loading />;
 
 =======
+=======
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+>>>>>>> ebac5ed (Add Loading component and apply across map, add-spot, modal, and navbar)
     const router = useRouter();
 
     React.useEffect(() => {
         const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            setUser(JSON.parse(storedUser));
-        }
+        if (storedUser) setUser(JSON.parse(storedUser));
+        setIsLoading(false);
     }, []);
 
     const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -75,6 +84,7 @@ if (isLoading) return <Loading />;
         setUser(null);
         router.push('/login');
     };
+if (isLoading) return <Loading />;
 
 >>>>>>> 776b990 (Add Cloudinary fallback avatar support and dynamic NavBar user state)
     return (
