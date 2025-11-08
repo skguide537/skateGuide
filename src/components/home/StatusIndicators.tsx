86efe0142c1b62a5cb250eff37ccda9364ffa199
Omri <irmoomri@gmail.com>
2025-11-08 14:10:27 +0200

    reduce spacing between cards, removing status for virtual grid on home and map page

--- src/components/home/StatusIndicators.tsx
+++ src/components/home/StatusIndicators.tsx
@@ -29,7 +29,6 @@ export default function StatusIndicators({ lastUpdated, parksWithDistance, parks
                 </Typography>
             </Box>
 
-            {/* Virtual Scrolling Status */}
            
         </>
     );
